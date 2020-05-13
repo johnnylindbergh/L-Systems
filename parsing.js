bb30d8e32887167327ee33e30db89442133b6535
Thomas Castleman <thomas7rex@gmail.com>
2020-05-13 17:35:58 -0400

    Implement a help boi for parsingg

--- parsing.js
+++ parsing.js
@@ -1,6 +1,6 @@
 
 // parses the text from the production rules
-function parseProductionRules(rawText) {
+function parseProductionRules(rawText, cb) {
   /*
     Constructs hashtable of the following form:
     {
@@ -9,15 +9,13 @@ function parseProductionRules(rawText) {
     }
   */
 
-  const split = rawText.split("\n");
-
-
-
-
+  parseRawText(rawText, (err, extractedRules) => {
+    cb(err, extractedRules);
+  });
 }
 
 // parses the text from the actions definitions
-function parseActions(rawText) {
+function parseActions(rawText, cb) {
   /*
     Constructs hashtable of the following form:
     {
@@ -29,17 +27,52 @@ function parseActions(rawText) {
   */
 }
 
+function parseRawText(rawText, cb) {
+  if (rawText == "") return cb(new Error("There were no production rules entered!"));
+
+  const split = rawText.split("\n");
+  const ret = [];
+
+  for (let i = 0; i < split.length; i++) {
+    let line = split[i];
+    let splitByColon = line.replace(/\s/g, '').split(":");
+
+    if (line == '') continue; // ignore blank lines
+
+    if (splitByColon.length != 2) {
+      return cb(new Error(`The line "${line}" had an incorrect amount of colons`));
+    }
+
+    if (!splitByColon[0] || !splitByColon[1]) {
+      return cb(new Error(`The line "${line}" errored: Both the left-hand side and right-hand side must be non-empty`));
+    }
+
+    ret.push({
+      LHS: splitByColon[0],
+      RHS: splitByColon[1]
+    });
+  }
+
+  cb(null, ret);
+}
+
 
 
 
 /* -------------------------------------------- */
 
 const testRules = 
-  `F : F-G+F+G-F
-  G : GG
-  test : wowza`;
+`F : F-G+F+G-F
 
-console.log(parseProductionRules(testRules));
+G : GG
+
+
+test : wowza`;
+
+parseProductionRules(testRules, (err, data) => {
+  console.log(err);
+  console.log(data);
+});
 
 
 const testActions =
